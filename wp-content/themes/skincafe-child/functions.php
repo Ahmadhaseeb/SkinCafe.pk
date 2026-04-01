32f842422e4dcee12c5d989404d479cbd00613c6
Ahmad Haseeb <a.haseeb330@gmail.com>
2026-04-02 00:23:42 +0500

    done

--- wp-content/themes/skincafe-child/functions.php
+++ wp-content/themes/skincafe-child/functions.php
@@ -22,3 +22,23 @@ function skincafe_child_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'skincafe_child_enqueue_styles', 15 );
+
+/**
+ * Add Product Search Bar to Astra Header
+ */
+function skincafe_add_header_product_search() {
+    if ( class_exists( 'WooCommerce' ) ) {
+        echo '<div class="skincafe-header-search-container">';
+        get_product_search_form();
+        echo '</div>';
+    }
+}
+// Hook into Astra's top header to ensure it displays prominently
+add_action('astra_masthead_top', 'skincafe_add_header_product_search');
+
+// Also provide a shortcode just in case they prefer to use Astra's drag-and-drop Header Builder widget
+add_shortcode('skincafe_product_search', function() {
+    ob_start();
+    get_product_search_form();
+    return '<div class="skincafe-header-search-container">' . ob_get_clean() . '</div>';
+});
